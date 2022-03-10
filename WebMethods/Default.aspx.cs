a12eda1d7873f09b411847241f01750a38adde35
Eliseo Gonzalez <eliseog@herbalife.com>
2022-03-10 15:14:35 -0600

    first commit

---
+++ WebMethods/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebMethods
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static string Test(string name)
+        {
+            return $"OK {name}";
+        }
+    }
+}
